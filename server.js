da8ed1d6caf033fddae3e947f13ca1bc01129921
Tara Thierry <tarathierry@gmail.com>
2022-10-19 21:37:17 -0400

    add: Profile model, route, controller

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const connectDB = require("./config/database");
 
 //routes
 const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts");
+const profileRoutes = require("./routes/profiles");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -58,7 +58,7 @@ app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-app.use("/post", postRoutes);
+app.use("/profile", profileRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
