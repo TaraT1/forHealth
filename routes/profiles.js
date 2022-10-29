327324bdf6cb33c475a1632bf6199f96aa2aa871
Tara Thierry <tarathierry@gmail.com>
2022-10-28 22:21:00 -0400

    add: Post, Get for profiles route

---
+++ routes/profiles.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const profilesController = require("../controllers/profiles");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Profile Routes - simplified for now
+router.get("/:id", ensureAuth, profilesController.getProfile);
+
+router.post("/createProfile", upload.single("file"), profilesController.createProfile);
+
+// router.put("/editProfile/:id", profilesController.editProfile);
+
+// router.delete("/deleteProfile/:id", profilesController.deleteProfile);
+
+module.exports = router;
