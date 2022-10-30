8462f9f9b1f9254dbb41b7328057ea80a2ca7622
Tara Thierry <tarathierry@gmail.com>
2022-10-30 00:14:53 -0400

    Add db connection check

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.DB_STRING);
 
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) { 
         console.error(err);
         process.exit(1);
