11f7190000551bd3d5f8d28660082aed1415b929
Tara Thierry <tarathierry@gmail.com>
2023-02-25 21:27:52 -0500

    fix: add provider routes to server

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const connectDB = require("./config/database");
 //routes
 const mainRoutes = require("./routes/main");
 const profileRoutes = require("./routes/profiles");
+const providerRoutes = require("./routes/providers");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -69,6 +70,7 @@ app.use(flash());
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
 app.use("/profiles", profileRoutes);
+app.use("/providers", providerRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
