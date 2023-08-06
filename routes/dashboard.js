684e405b9b49a4a1021e2eae720c56bb9e90f5e5
Tara Thierry <tarathierry@gmail.com>
2023-08-06 09:09:22 -0400

    Cleanup refs

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router(); 
 const dashboardController = require('../controllers/dashboard');
-//const authController = require("../controllers/auth");
+const authController = require("../controllers/auth");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Dashboard Routes
--- routes/main.js
+++ routes/main.js
@@ -3,13 +3,13 @@ const router = express.Router();
 // const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const profilesController = require("../controllers/profiles");
-const dashboardController = require("../controllers/dashboard")
+// const dashboardController = require("../controllers/dashboard")
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes
-router.get("/", homeController.getIndex);
+router.get("/", ensureGuest, homeController.getIndex);
 router.get("/profiles", ensureAuth, profilesController.getProfiles);
 // router.get("/providers", providersController.getProviders);
-router.get("/dashboard", dashboardController.getDashboard);
+// router.get("/dashboard", dashboardController.getDashboard);
 
 module.exports = router;
