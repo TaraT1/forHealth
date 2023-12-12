b50119ca0bc9a964d8010c538ee0aa817e2312c9
Tara Thierry <tarathierry@gmail.com>
2023-12-11 20:31:22 -0500

    minor fixes

--- routes/healthInfo.js
+++ routes/healthInfo.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const upload = require("../middleware/multer")
 const healthInfoController = require("../controllers/healthInfo")
 const { ensureAuth, ensureGuest } = require("../middleware/auth")
 
