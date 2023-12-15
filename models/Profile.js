9e21c320923d02d28d481bcf5380e5f28da61482
Tara Thierry <tarathierry@gmail.com>
2023-12-15 08:48:44 -0500

    Add fixes and formatting

--- models/Profile.js
+++ models/Profile.js
@@ -1,71 +1,77 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ProfileSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  /*
- fullName: {
-  fName,
-  lName,
- }, 
- contact: {
-  phone: String,
-  email: String
-},
-  address: {
-    street: String,
-    city: String
- },
-*/
-  birthDate: {
-    type: Date,
-    required: false, 
-  },
-  bloodType: {
-    type: String,
-    require: false,
-  },
-  eHealthRecords: {
-    type: String,
-    require: false,
-  },
-  insurance: {
-    type: String,
-    require: false,
-  },
-  inCaseOfEmergency: {
+const ProfileSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    name: {
       type: String,
+      required: true,
+    },
+    /*
+    fullName: {
+      fName,
+      lName,
+    }, 
+    contact: {
+      phone: String,
+      email: String
+    },
+      address: {
+        street: String,
+        city: String
+    },
+    */
+    birthDate: {
+      type: Date,
+      required: false,
+    },
+    bloodType: {
+      type: String,
+      require: false,
+    },
+    eHealthRecords: {
+      type: String,
+      require: false,
+    },
+    insurance: {
+      type: String,
+      require: false,
+    },
+    inCaseOfEmergency: {
+      type: String,
+    },
+    journal: {
+      type: String,
+      require: false,
+    },
+    image: {
+      type: String,
+      require: false,
+    },
+    healthInfo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "HealthInfo",
+    },
+    providers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Provider",
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      immutable: true,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  journal: {
-    type: String,
-    require: false,
-  },
-  image: {
-    type: String,
-    require: false,
-  },
-  providers: 
-    [{
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'Provider' 
-    }],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    immutable: true,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-},
-{ timestamps: true}
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('Profile', ProfileSchema);
+module.exports = mongoose.model("Profile", ProfileSchema);
